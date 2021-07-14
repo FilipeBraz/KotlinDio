3e36806a1424ed8f21d653e06eb34d71e55908f1
brazz <filipe_braz@hotmail.com>
2021-07-13 23:22:59 -0300

    updated to talk about class members

--- src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kts
+++ src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kts
@@ -2,10 +2,13 @@ class Pessoa {
     var nome: String = "Brazz"
     var cpf: String = "987.456.897-00"
     private set
+    
+    constructor()
+
+    fun pessoaInfo() = "$nome + $cpf"
 }
 
 fun main(){
     val filipe = Pessoa()
-    println(filipe.nome)
-    println(filipe.cpf)
+    println(filipe.pessoaInfo())
 }
