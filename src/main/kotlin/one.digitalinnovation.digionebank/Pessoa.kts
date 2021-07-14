9d573e5c7dbf390cc58653ca3c5f45c5ad813ae3
brazz <filipe_braz@hotmail.com>
2021-07-13 22:39:34 -0300

    pessoa add dados

---
+++ src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kts
@@ -0,0 +1,11 @@
+class Pessoa {
+    var nome: String = "Brazz"
+    var cpf: String = "987.456.897-00"
+}
+
+fun main(){
+    var filipe = Pessoa()
+
+    println(filipe.nome)
+    println(filipe.cpf)
+}
